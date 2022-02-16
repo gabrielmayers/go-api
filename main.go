0f4ec668fcf0d3c22a9949d0afcb297a1ff51744
gabrielmayers <gabriel.mayers@hotmail.com>
2022-02-15 22:00:29 -0300

    first files

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200,
+			gin.H{
+				"message": "GO API baby",
+			})
+	})
+
+	router.GET("/os", func(c *gin.Context) {
+		c.String(200, runtime.GOOS)
+	})
+
+	router.Run()
+}
